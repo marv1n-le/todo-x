706476b5cdd41aa4f7c695efe90bd8a8f2ff3fdb
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-10-30 21:07:13 +0700

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="space-y-2 text-center">
       <h1 className="text-4xl font-bold text-transparent bg-primary bg-clip-text">
-        TodoX
+        Mai Phuong Task Manager
       </h1>
       <p className="text-muted-foreground">
         Hổng có việc gì khó, chỉ sợ mình hổng làm 💪
